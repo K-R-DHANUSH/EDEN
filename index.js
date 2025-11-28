fe134940f1594113aba43f94d28083ad313558fc
DHANUSH K R <81808160+K-R-DHANUSH@users.noreply.github.com>
2025-11-29 02:58:10 +0530

    Implement theme toggle feature
    Added theme toggle functionality to switch between light and dark themes.

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// MENU
 const navToggle = document.getElementById("navToggle");
 const navMenu = document.getElementById("navMenu");
 
 navToggle.addEventListener("click", () => {
     navMenu.classList.toggle("active");
+});
+
+// THEME
+const themeBtn = document.getElementById("themeToggle");
+
+function setThemeIcon() {
+    const isDark = document.body.classList.contains("dark-theme");
+    themeBtn.textContent = isDark ? "🌙" : "☀️";
+}
+
+setThemeIcon();
+
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-theme");
+    setThemeIcon();
 });
